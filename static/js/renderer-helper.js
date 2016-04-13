ff58e686c56959e8fe4623a150e01a58240fb6ef
sam23d <samuel.drach@gmail.com>
2016-04-13 17:11:22 +0300

    fixed center content to 100% with scroll

---
+++ static/js/renderer-helper.js
@@ -0,0 +1,6 @@
+
+var mainWrapper = document.getElementById("main-wrapper");
+
+var containerHeight = window.innerHeight - 64 ;
+console.log(containerHeight);
+mainWrapper.style.height = containerHeight.toString() + "px" ;
