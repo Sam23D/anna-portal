c119065b7a0b1e2554f3a7a19392e4fa64333055
sam23d <samuel.drach@gmail.com>
2016-05-09 11:23:21 +0300

    project transition

--- static/js/test.js
+++ static/js/test.js
@@ -2,20 +2,69 @@ var mainContent = $("#main-content");
 
 var projectsBtn = $("#btn-projects");
 var profileBtn = $("#btn-profile");
-var notificationssBtn = $("#btn-notifications");
+var notificationsBtn = $("#btn-notifications");
 
-var testProjects = $("#test-projects");
 var projectsContainer = $("#projects-container");
 var singleProject = $("#project-container");
 
-//Views
 
-var clearMainContainer = function(){
+var main = function(){
+  clearMainContainer();
+}
+
+//#########################################################################VIEWS
+//#########################################################################VIEWS
+//#########################################################################VIEWS
+var allContentViews = [
+  singleProject,
+  projectsContainer
+]
+
+//button event actions bindings
+projectsBtn.on("click",function(){
+  clearMainContainer();
+  projectsContainer.show();
+});
+notificationsBtn.on("click", function(){
+  clearMainContainer();
+
+});
+
+
+
+var clearMainContainer = function(){ // sets all views in the container to hidden
+  allContentViews.forEach(function(view){
+    view.hide();
+  })
+}
+
+
+//###################################################################CONTROLLERS
+//###################################################################CONTROLLERS
+//###################################################################CONTROLLERS
+
+var SingleProjectController = {
+  getProject : function(){
+   // request project with id from server
+ },
+ renderProject : function( ){
+   //fill single project container
+   console.log("dam")
+   clearMainContainer();
+   singleProject.show();
+ }
 
 }
 
-//Controllers
+//########################################################################MODELS
+//########################################################################MODELS
+//########################################################################MODELS
+var Project = {
+  title : "Test Project",
+  img : "https://d.ibtimes.co.uk/en/full/1477372/syria-2015-photos.jpg",
+  content :`  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat`
+}
 
-
-
-//Models
+//MAIN
+main();
