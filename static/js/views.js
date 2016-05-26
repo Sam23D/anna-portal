89233780301fed3f82089764409bcdb50b147b2b
sam23d <samuel.drach@gmail.com>
2016-05-26 02:44:04 -0600

    added award and featured project template

--- static/js/views.js
+++ static/js/views.js
@@ -1,5 +1,5 @@
-
-const  profileCard = _.template(
+//{imgUrl: , name: , country: , title: }
+const  menuProfileCard = _.template(
   `<div class="col s6">
       <img id="profile-picture" src="<%= imgUrl %>">
     </div>
@@ -9,7 +9,8 @@ const  profileCard = _.template(
       <p><%= title %></p>
     </div>`);
 
-const profileScore = _.template(
+//{credits: , points: }
+const menuProfileScore = _.template(
   `<div class="row">
       <div class="col l6">
         <p>Credits:</p>
@@ -20,3 +21,33 @@ const profileScore = _.template(
         <p class="points-badge"><%= points %></p>
       </div>
   </div>`);
+
+//{details: , imgUrl: }
+const menuAward = _.template(
+  `<div class="col l6">
+    <a class=" tooltipped" data-position="top" data-delay="50" data-tooltip="<%= details %>">
+      <img class="award" src="<%= imgUrl %>" alt="" >
+    </a>
+  </div>`
+);
+
+//{imgUrl: , title: , progress: ,goalDetails: , description: , }
+const feturedProject = _.template(
+  `<div class="col l12">
+    <div class="card medium">
+        <div class="card-image">
+          <img src="<%= imgUrl %>">
+        </div>
+
+        <div class="card-content">
+          <a href="#" onclick=" SingleProjectController.renderProject() "><h5><%= title %></h5></a>
+          <div class="progress">
+              <div class="determinate" style="width: <%= progress %>%"></div>
+          </div>
+          <span class="project-details">Goal: <%= goalDetails %>/span>
+          <p><%= description %></p>
+        </div>
+
+    </div>
+  </div>`
+);
